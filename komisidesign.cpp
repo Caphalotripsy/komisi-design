33c09801eb7a180515e1b38e9a5d6bd74d8282e5
Caphalotripsy <imamnawwi123@gmail.com>
2025-06-20 05:09:21 +0800

    Penambahan head untuk pointer dan fungsi untuk membuat node baru yang akan ditambahkan ke linked list

--- komisidesign.cpp
+++ komisidesign.cpp
@@ -13,11 +13,44 @@ struct Order {
     Order* next;
 };
 
+// Pointer head linked list
+Order* head = nullptr;
+int nextId = 1;
+
+// Fungsi untuk membuat node baru dan menambahkannya ke linked list
+void tambahOrder() {
+    Order* newOrder = new Order;
+    newOrder->id = nextId++;
+    cin.ignore();
+    cout << "Nama Klien: ";
+    getline(cin, newOrder->namaKlien);
+    cout << "Jenis Desain (logo/poster/ilustrasi/dll): ";
+    getline(cin, newOrder->jenisDesain);
+    cout << "Deskripsi Singkat: ";
+    getline(cin, newOrder->deskripsi);
+    cout << "Harga (Rp): ";
+    cin >> newOrder->harga;
+    newOrder->next = nullptr;
+
+    // Tambah ke akhir linked list
+    if (head == nullptr) {
+        head = newOrder;
+    } else {
+        Order* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newOrder;
+    }
+
+    cout << "Order berhasil ditambahkan!\n";
+}
+
 // Variabel global untuk menyimpan semua order
 vector<Order> daftarOrder;
 int nextId = 1;
 
     int main() {
-        
+
     return 0;
 }
